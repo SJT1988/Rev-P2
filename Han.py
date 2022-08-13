a1101efe652d5f2d93e229523e6b96a8b4b74b85
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-13 18:19:59 -0500

    updating

--- Han.py
+++ Han.py
@@ -2,8 +2,14 @@
 # Visual Studio Python 3
 
 import csv, re
+import random
 
 class productInfo():
     # Generate at least a 1000 fake(?) products and product categories
 
     @staticmethod
+    def fakeProducts():
+        num = 1000
+        idNum = 0
+        with open('product_info.csv', 'w', encoding='utf-8', newline = '') as file:
+            
