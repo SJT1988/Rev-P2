4fb546e118ca219221acb0063d6ac5cc4ef34af6
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-14 14:26:09 -0500

    update

--- Han.py
+++ Han.py
@@ -4,18 +4,18 @@
 import csv
 import random
 
-class productInfo():
-    # Generate at least a 1000 fake(?) products and product categories
 
-    def product_id(num):
-        lst = []
-        for i in range(num):
-            id = random.randint(10000, 100000)
-            if id not in lst:
-                lst.append(id)
-            else:
-                i -= 1
-        return lst
+# Generate at least a 1000 fake(?) products and product categories
+
+def product_id(num):
+    lst = []
+    for i in range(num):
+        id = random.randint(10000, 100000)
+        if id not in lst:
+            lst.append(id)
+        else:
+            i -= 1
+    return lst
     
     # Tech
 
