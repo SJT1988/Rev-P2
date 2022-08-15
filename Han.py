a20bc7a55a23e977c4a6b7e2fb86581393719fa7
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-15 18:07:42 -0500

    closer update

--- Han.py
+++ Han.py
@@ -8,6 +8,42 @@ import random
 
 
 # Generate at least a 1000 fake(?) products and product categories
+tech = ['Laptops', 'Hard Drives', 'Phones', 'Microwaves', 'Headphones', 'Electronic Locks', 'Drones', 'Computers', 'Tablets', 'TVs', 'Remotes', 'Earbuds',
+                'Smart fridges', 'Mouse', 'Webcam', 'USB', 'Cables']
+
+clothes = ['Jeans', 'Underwear', 'Shoes', 'Sandals', 'Formal Wear', 'Socks', 'Swimwear', 'Kahkis', 'Gowns', 'Dresses', 'T-shirts', 'Glasses', 'Sunglasses']
+
+furniture = ['Chairs', 'Couches', 'Recliners', 'Tables', 'Cabinets', 'Clocks', 'Bookcase', 'Dresser', 'Chest', 'Beds']
+
+school = ['Notebooks', 'Pencils', 'Mechanical Pencils', 'Erasers', 'Chromebooks', 'Textbooks', 'Backpacks', 'Bags', 'Rulers']
+
+dict_type = {tech:['Laptops', 'Hard Drives', 'Phones', 'Microwaves', 'Headphones', 'Electronic Locks', 'Drones', 'Computers', 'Tablets', 'TVs', 'Remotes', 'Earbuds',
+                'Smart fridges', 'Mouse', 'Webcam', 'USB', 'Cables'], clothes:['Jeans', 'Underwear', 'Shoes', 'Sandals', 'Formal Wear', 'Socks', 'Swimwear', 'Kahkis', 
+                'Gowns', 'Dresses', 'T-shirts', 'Glasses', 'Sunglasses'], furniture:['Chairs', 'Couches', 'Recliners', 'Tables', 'Cabinets', 'Clocks', 'Bookcase', 'Dresser', 
+                'Chest', 'Beds'], school:['Notebooks', 'Pencils', 'Mechanical Pencils', 'Erasers', 'Chromebooks', 'Textbooks', 'Backpacks', 'Bags', 'Rulers']}
+
+lastproduct_selection = random.choice(list(dict_type))
+product_type = last_selection[0]
+
+def product():
+    n = 1500
+    id = 0
+    with open('product_info.csv', 'w', encoding='utf-8', newline='') as file
+        writer = csv.writer(file)
+        writer.writerow(['ID', 'Product', 'Category'])
+
+        for i in range(n):
+            
+
+
+
+
+
+
+
+
+
+
 
 
 
