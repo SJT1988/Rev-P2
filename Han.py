190b27552449e6747673a6b8cd59cb418f4f9541
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-15 13:34:56 -0500

    list update

--- Han.py
+++ Han.py
@@ -4,20 +4,23 @@
 import csv
 import random
 
+# PRODUCT ID, PRODCUT NAME, PRODUCT CATEGORY
+
 
 # Generate at least a 1000 fake(?) products and product categories
 
-def product_id(num):
-    lst = []
-    for i in range(num):
-        id = random.randint(10000, 100000)
-        if id not in lst:
-            lst.append(id)
-        else:
-            i += 1
-    return lst
-    
-# Tech / Food / Clothes / Beverages / Appliances / Home Decor / Sports
-def product_category():
-    category = random.randint(0, 105)
-    if
+
+
+'''
+list_size = ['tiny','little, 'medium', 'big', 'honkin\'']
+list_color = ['red','blue','green','orange']
+dict_type = {kitchen: ['toaster','microwave'],
+furniture: ['sofa','desk'],
+electronics: ['television','stereo'], fruit: ['pineapple','bannana']}
+
+last_selection = random.choice(list(dict_type))
+
+item_name=random.choice(list_size) + random.choice(list_color) + random.choice(last_selection[1])
+
+item_type = last_selection[0]
+'''
