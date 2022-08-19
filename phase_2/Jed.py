59dca90d6c1d3232271d70b2444a414dabc19a93
JedATix <65898875+JedATix@users.noreply.github.com>
2022-08-19 17:03:49 -0500

    Jed - Updated - Final Work with detailed comments notes and learnings
    A detailed account of problems faced and associated learnings are included into the body of the py file as comments

--- phase_2/Jed.py
+++ phase_2/Jed.py
@@ -49,42 +49,69 @@ df_Q1_1.createOrReplaceTempView("mydata")
 #
 #################################################################################################################
 
+
 df_Q1_2 = spark.sql("select count(product_category) as mycount, product_category, country from mydata group by product_category,country order by country asc, mycount desc ")
 df_Q1_2.show(10) # the show statement diplays the results. within the brackets you can define the number of records you want to see
 
 df_Q1_2.createOrReplaceTempView("P1")
 
+'''
+########################################################################################################################################
+LEARNING
 
-# df_Q1_3 = spark.sql("select mycount as mycount, product_category, country from P1 group by country having \
-#     mycount = max(select max(mycount) as mycount from P1 group by country) \
-# order by country")
+spark sql is slightly different to mysql
+in my experince 
+In my experience, you cannot execute the same complex mysql queries in pyspark for example for Q1 in mysql you can run the following 
+command for a data table with the headings [‘mycoint’, ‘product_category’, ‘country’]
 
-df_Q1_3_1 = spark.sql("select max(mycount) as my_count, product_category, country  from P1 group by country order by country")
-#df_Q1_3.createOrReplaceTempView("Q1_Final")
-df_Q1_3_1.show(10)
-#df_Q1_3_1.createOrReplaceTempView("q1_1") 
+select max(mycount), product_category, country from t1 group by country order by country; 
 
-#df_n = spark.sql("select * from q1_1")
-#df_n.show(10)
+if you run the above mysql query in pyspark.sql it will warn you that product_category is not an aggregate function or is not within
+the group by clause.If you include product_category into the group by clause you will have the same data frame as seen above
 
-# df_Q1_3_2 = spark.sql("select q1_1.mycount, \
-#                         q1_1.country, \
-#                         P1.product_category from q1_1 inner join p1 on \
-#                         (p1.country = q1_1.country) \
-#                         inner join p1 on \
-#                         (p1.mycount = q1_1.mycount) order by country")
-# df_Q1_3_2.show(10)
+So,
+I used the following command and did a join
+
+select max(mycount) as mycount_n, country as country_n from P1 group by country order by country 
+
+this give the below result 
++---------+----------+
+|mycount_n| country_n|
++---------+----------+
+|        7|    Angola|
+|       80| Argentina|
+|       12| Australia|
+|        8|   Austria|
+|       93|Bangladesh|
+|       11|   Bolivia|
+|      199|    Brazil|
+|       10|    Canada|
+|       10|     Chdna|
+|        7|     Chile|
+
+The benefit of providing column aliases is important to prevent column ambiguity in joining columns and later on selecting the
+right columns from the joined table 
+
+'''
+df_Q1_3_1 = spark.sql("select max(mycount) as mycount_n, country as country_n from P1 group by country order by country")
+df_Q1_3_1.show(10) # to display results
 
 
-# df_Q1_3_3 = spark.sql()
-# df_Q1_Final = spark.sql("show tables") # this is to see if the tables exists
-# df_Q1_Final.show()
+df_Q1_3_2 = df_Q1_3_1.join(df_Q1_2,[df_Q1_2.mycount == df_Q1_3_1.mycount_n, df_Q1_2.country == df_Q1_3_1.country_n], 'inner')
+df_Q1_3_2.show()
+'''
+
+You can observe the benefit of proving different column aliases in here 
+
+'''
+df_Q1_3_3 = df_Q1_3_2.select([df_Q1_3_2.mycount, df_Q1_3_2.product_category, df_Q1_3_2.country]).orderBy(df_Q1_3_2.country)
+df_Q1_3_3.show()
+
 #    writing the results to a csv file
 #    in here you can directly write the data frame into a csv file 
 #    we can also convert this to an RDD and write it into a csv file. However, this requires aditional steps
 
-#df_Q1_Final = spark.sql("select * from q1_final")
-#df_Q1_Final.show(10)
+df_Q1_3_3.write.csv('file:/home/jed/out.csv')
 
 print ("CSV Printed")
 #                               End of Q1 for P2    
