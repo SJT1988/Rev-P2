01bf5b942c1a21556664d7cc91e1b11b8daf7b46
SJT1988 <strumbore88@gmail.com>
2022-08-22 04:43:27 -0700

    completed first query

--- phase_2/spencer_2.py
+++ phase_2/spencer_2.py
@@ -114,5 +114,33 @@ df_sum_of_categories = spark.sql('SELECT product_category, SUM(totals) AS cat_su
 # The maximum record is the first ordered record:
 highest_grossing_cat_list = list(df_sum_of_categories.collect()[0])
 print(f'1a) The highest grossing product category overall is {highest_grossing_cat_list[0]} with {"{:,}".format(highest_grossing_cat_list[1])}')
+print('\n\n\n')
+
+#################################################################################################################
+#
+#               1b) What is the top selling category of items per country?
+#
+#################################################################################################################
+
+df_category_totals = df_typecast.select('product_category', (df_typecast.qty*df_typecast.price).alias('totals'), 'country')
+df_category_totals.createOrReplaceTempView('category_totals')
+df_sum_of_categories = spark.sql('SELECT product_category, SUM(totals) AS cat_sums, country FROM category_totals GROUP BY country, product_category ORDER BY country, cat_sums DESC')
+# df_sum_of_categories.show(150)
+df_sum_of_categories.createOrReplaceTempView('category_totals')
+# use a common table expression (CTE) to filter rows
+df_gross_by_cat_by_country = spark.sql('WITH max_categories AS ( \
+    SELECT MAX(cat_sums) as max_gross, country \
+    FROM category_totals \
+    GROUP BY country\
+) \
+SELECT category_totals.product_category, max_categories.max_gross, category_totals.country FROM category_totals \
+INNER JOIN max_categories \
+ON max_categories.max_gross = category_totals.cat_sums \
+AND max_categories.country = category_totals.country \
+ORDER BY max_categories.max_gross DESC;')
+print('1b) The highest grossing product categories by country are:')
+df_gross_by_cat_by_country.show(100)
+print('\n\n\n')
+
 # in console type spark-submit pythonfilename.py
 spark.stop()
